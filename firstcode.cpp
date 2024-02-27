aac09165d0110e2f19ac651ff574fe9e0839d335
Barry Gilson <161463284+claudebar@users.noreply.github.com>
2024-02-27 13:52:14 +0200

    Create firstcode.cpp

---
+++ firstcode.cpp
@@ -0,0 +1,24 @@
+#include "geometry.h"
+
+vec3 cross(const vec3 &v1, const vec3 &v2) {
+    return vec<3>{v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x};
+}
+
+  if (in.fail()) return;
+    std::string line;
+    while (!in.eof()) {
+        std::getline(in, line);
+        std::istringstream iss(line.c_str());
+        char trash;
+        if (!line.compare(0, 2, "v ")) {
+            iss >> trash;
+            vec3 v;
+            for (int i=0;i<3;i++) iss >> v[i];
+            verts.push_back(v);
+
+void viewport(const int x, const int y, const int w, const int h) {
+    Viewport = {{{w/2., 0, 0, x+w/2.}, {0, h/2., 0, y+h/2.}, {0,0,1,0}, {0,0,0,1}}};
+}
+
+void projection(const double f) {
+    Projection = {{{1,0,0,0}, {0,-1,0,0}, {0,0,1,0}, {0,0,-1/f,0}}};
